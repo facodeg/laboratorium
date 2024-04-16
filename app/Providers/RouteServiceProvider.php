6d80baaf63f2ea733ea6b0c907670217095052ec
sigit4715 <bowor4751@gmail.com>
2024-04-16 09:57:01 +0700

    s

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,30 +30,28 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+            Route::middleware('api')->prefix('api')->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+            Route::middleware('web')->group(base_path('routes/web.php'));
 
-             // Mengarahkan pengguna ke rute yang sesuai berdasarkan peran
-             Route::middleware('web')
-             ->group(function () {
-                 Route::get('/home', function () {
-                     $user = Auth::user();
+            // Mengarahkan pengguna ke rute yang sesuai berdasarkan peran
+            Route::middleware('web')->group(function () {
+                Route::get('/home', function () {
+                    $user = Auth::user();
 
-                     if ($user->role == 'admin') {
-                         return redirect()->route('admin.home');
-                     } elseif ($user->role == 'user') {
-                         return redirect()->route('user.home');
-                     } else {
-                         Auth::logout();
-                         flash('Anda tidak memiliki hak akses')->error();
-                         return redirect()->route('login');
-                     }
-                 })->name('home');
-             });
+                    if ($user->role == 'admin') {
+                        return redirect()->route('admin.home');
+                    } elseif ($user->role == 'user') {
+                        return redirect()->route('user.home');
+                    } elseif ($user->role == 'perawat') {
+                        return redirect()->route('perawat.home');
+                    } else {
+                        Auth::logout();
+                        flash('Anda tidak memiliki hak akses')->error();
+                        return redirect()->route('login');
+                    }
+                })->name('home');
+            });
         });
     }
 }
